6008b1e24626e3498b6d74d0e6910728da0b7ca0
majortom327 <me@valentin-thomas.com>
2023-06-29 17:40:10 +0200

    Doc crema et civility

--- app/routes/sheets.civility/index.tsx
+++ app/routes/sheets.civility/index.tsx
@@ -1,10 +1,16 @@
 import { Badge } from "react-daisyui";
+import { FaChevronRight } from "react-icons/fa";
+import RouteEnum from "~/refs/enum/route";
 
-import CustomCard, { CustomCardTitle } from "~/components/CustomCard";
+import { ButtonLink } from "~/components/ButtonLink";
+import CustomCard, {
+  CustomCardActions,
+  CustomCardTitle,
+} from "~/components/CustomCard";
+import Bolder from "~/components/Documentation/Bolder";
 import DocImage from "~/components/Documentation/DocImage";
+import Paragraph from "~/components/Documentation/Paragraph";
 import ErrorHandler from "~/components/ErrorHandler";
-
-
 
 import civilityFormImg from "./civility-form.png";
 
@@ -17,7 +23,32 @@ export const SheetsCivility = () => {
           <Badge>Défunt uniquement</Badge>
         </CustomCardTitle>
 
+        <Paragraph>
+          La définition de la civilitée est une etape importante dans le cadre
+          de fiche de defunt. Elle est divisée en plusieurs groupe. On y trouve
+          la partie civilite pure permetant de definir les informations basique
+          tel que le nom, le prenom, le nom par alliance ainsi que la civilitée
+          de la personne.
+          <br />
+          Suivi par deux groupes avec les informations sur la naissance du
+          défunt ainsi que son décès. Cela permet nottament de remplir le
+          certificat de crémation.
+          <br />
+          <Bolder>
+            Cette partie n'est pas disponible lors-ce que la fiche est associée
+            a un reliquaire.
+          </Bolder>
+        </Paragraph>
+
         <DocImage src={civilityFormImg} />
+        <CustomCardActions>
+          <div className="flex w-full justify-end">
+            <ButtonLink color="primary" to={RouteEnum.sheetCremation}>
+              Crémation
+              <FaChevronRight className="ml-2" />
+            </ButtonLink>
+          </div>
+        </CustomCardActions>
       </CustomCard>
     </>
   );
--- app/routes/sheets.cremation/index.tsx
+++ app/routes/sheets.cremation/index.tsx
@@ -1,6 +1,10 @@
+import RouteEnum from "~/refs/enum/route";
+
 import { CustomCardTitle } from "~/components/CustomCard";
 import CustomCard from "~/components/CustomCard/CustomCard";
+import DocLink from "~/components/DocLink/DocLink";
 import DocImage from "~/components/Documentation/DocImage";
+import Paragraph from "~/components/Documentation/Paragraph";
 import ErrorHandler from "~/components/ErrorHandler";
 
 import addCommanditaireImg from "./add-commanditaire.png";
@@ -12,8 +16,39 @@ export const SheetsCremation = () => {
       <CustomCard>
         <CustomCardTitle>Crémation d'une fiche</CustomCardTitle>
 
+        <Paragraph>
+          La qualification de la crémation contient d'autre champs plus
+          technique. On y retrouve la personne en charge de la fiche. Cette
+          personne est selectionnable dans la liste des{" "}
+          <DocLink to={RouteEnum.societyUsers}>
+            utilisateurs de la société
+          </DocLink>
+        </Paragraph>
+
         <DocImage src={cremationFormImg} />
+
+        <Paragraph>
+          Le commanditaire est un champs plus complexe, il permet d'associer a
+          un client la fiche. On peut en creer un a la vollée ou en selectionner
+          un existant dans la liste.
+        </Paragraph>
         <DocImage src={addCommanditaireImg} />
+
+        <Paragraph>
+          On peut enfin retrouver la destination des cendres qui est un champs
+          selectionnable depuis la liste definie dans les{" "}
+          <DocLink to={RouteEnum.societyDestinations}>
+            parametres de la societe
+          </DocLink>
+        </Paragraph>
+        <Paragraph>
+          Enfin les informations complementaires sur l'autorisation de la
+          crémation contiennent le numero d'ordre, les informations sur
+          l'autorisation ainsi que le numero de l'acte de décès.
+          <br />
+          La qualification des informations sur la crémation, elle contient la
+          date et les heures de la crémation.
+        </Paragraph>
       </CustomCard>
     </>
   );
